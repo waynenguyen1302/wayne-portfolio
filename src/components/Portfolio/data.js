375dde1ba1211f777616525dc8bb6c6da18b436c
waynenguyen1302 <111101794+waynenguyen1302@users.noreply.github.com>
2022-11-28 19:31:57 -0800

    setup gh-pages

--- src/components/Portfolio/data.js
+++ src/components/Portfolio/data.js
@@ -1,28 +1,28 @@
 const Portfolios = [
     {
-        cover: "/portfolio/nirvana/nirvana-cover.png",
-        image:"/portfolio/nirvana/nirvana.png",
+        cover: "portfolio/nirvana/nirvana-cover.png",
+        image:"portfolio/nirvana/nirvana.png",
         title: "Nirvana Spa",
         description: "Wordpress, HTML, CSS",
         url: "nirvana/nirvana.png"
     },
     {
-        cover: "/portfolio/chayTO/chayTO-cover.png",
-        image:"/portfolio/chayTO/chayTO.png",
+        cover: "portfolio/chayTO/chayTO-cover.png",
+        image:"portfolio/chayTO/chayTO.png",
         title: "Chay Toronto",
         description: "Wordpress, HTML, CSS",
         url: "http://google.com"
     },
     {
-        cover: "/portfolio/dmc-college-project/dmc.png",
-        image:"/portfolio/dmc-college-project/dmc.png",
+        cover: "portfolio/dmc-college-project/dmc.png",
+        image:"portfolio/dmc-college-project/dmc.png",
         title: "DMC - College Project",
         description: "HTML, CSS, JavaScript",
         url: "http://google.com"
     },
     {
-        cover: "/portfolio/smoothie-on-earth/smoothie-on-earth.png",
-        image:"/portfolio/smoothie-on-earth/smoothie-on-earth.png",
+        cover: "portfolio/smoothie-on-earth/smoothie-on-earth.png",
+        image:"portfolio/smoothie-on-earth/smoothie-on-earth.png",
         title: "Smoothie On Earth",
         description: "HTML, CSS, JavaScript",
         url: "http://google.com"
