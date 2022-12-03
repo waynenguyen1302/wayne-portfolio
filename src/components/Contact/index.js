e4d53418f8ea0e58f795c87e99b41601c31fadf7
waynenguyen1302 <111101794+waynenguyen1302@users.noreply.github.com>
2022-12-02 19:41:46 -0800

    setup gh-pages

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -56,7 +56,7 @@ const Contact = () => {
                         />
                     </h1>
                     <p>
-                        Whether it's an opportunity, or you just want to talk. Send over your request and/or question, don't hesitate to contact me using the form below. I'll respond as soon as I can.
+                        Whether it's an opportunity, or you just want to talk. Send over your request and/or question, don't hesitate to contact me using the form below. 
                     </p>
                     <div className='contact-form'>
                         <form ref={refForm} onSubmit={sendEmail}>
