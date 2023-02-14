d956f9f538354bcd7ed250df660b1b9179e7526a
waynenguyen1302 <111101794+waynenguyen1302@users.noreply.github.com>
2023-02-14 02:03:25 -0800

    add new project booknow.com

--- src/components/Portfolio/portfolios.js
+++ src/components/Portfolio/portfolios.js
@@ -67,7 +67,7 @@ const Portfolios = () => {
                                 <div className="content" key={idx}>
                                     <p className="title">{port.title}</p>
                                     <h4 className="description">{port.description}</h4>
-                                    <Link to = {port.url}>
+                                    <Link to = {port.url} style={{textDecoration:"none"}}>
                                         <h4 className="description">{port.url}</h4>
                                     </Link>                                    
                                     <button className="btn" onClick={ () => handleOpenModal(idx)}>View</button>          
