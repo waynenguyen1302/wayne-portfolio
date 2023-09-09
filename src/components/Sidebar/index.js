0cb2dd3dc3a9c5d60b62be48a251387548dee3c1
waynenguyen1302 <quang.ndm91@gmail.com>
2023-09-09 11:42:05 -0700

    added git hub link to side bar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,7 +4,7 @@ import LogoW from  '../../assets/images/logo-w2.png'
 import LogoSubtitle from  '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faLightbulb, faUser, faWandMagicSparkles, faBars, faClose } from '@fortawesome/free-solid-svg-icons'
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { useState } from 'react'
 
 const Sidebar = () =>  {
@@ -44,6 +44,9 @@ const Sidebar = () =>  {
                 <li>
                     <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/wayne-d-nguyen/">
                         <FontAwesomeIcon icon = {faLinkedin} color="#4d4d4e" />
+                    </a>
+                    <a target="_blank" rel="noreferrer" href="https://github.com/waynenguyen1302">
+                        <FontAwesomeIcon icon = {faGithub} color="#4d4d4e" />
                     </a>
                 </li>
             </ul>
