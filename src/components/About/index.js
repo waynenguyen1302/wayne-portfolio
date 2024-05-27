2e0f10bf4d4004fae36ec13eb7d54d2f449a4906
waynenguyen1302 <quang.ndm91@gmail.com>
2024-05-26 23:16:42 -0400

    replace content

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,22 +2,22 @@ import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCss3, faGitAlt, faHtml5, faJsSquare, faPhp, faReact} from '@fortawesome/free-brands-svg-icons'
+import { faCss3, faGitAlt, faHtml5, faJsSquare, faPhp, faReact } from '@fortawesome/free-brands-svg-icons'
 import Loader from 'react-loaders'
 
 const About = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
     useEffect(() => {
-        
+
         let timeoutId = setTimeout(() => {
             setLetterClass('text-animate-hover')
         }, 4000)
-        
+
         return () => {
-                    clearTimeout(timeoutId)
-                }
+            clearTimeout(timeoutId)
+        }
     }, [])
-    
+
     return (
         <>
             <div className='container about-page'>
@@ -25,7 +25,7 @@ const About = () => {
                     <h1>
                         <AnimatedLetters
                             letterClass={letterClass}
-                            strArray={['A','b','o','u','t',' ','m','e']}
+                            strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
                             idx={15}
                         />
                     </h1>
@@ -33,22 +33,23 @@ const About = () => {
                         Hello, this is Wayne, an aspiring full-stack developer with a passion for crafting digital solutions to real-world challenges. I've embarked on an exciting journey, expanding my skills from front-end development into the realms of full-stack development. My goal is to create elegant, user-centric solutions that not only look great but also function seamlessly.
                     </p>
                     <h2>
-                        <AnimatedLetters 
+                        <AnimatedLetters
                             letterClass={letterClass}
-                            strArray={['A',' ','C','r','e','a','t','i','v','e',' ','J','o','u','r','n','e','y']}
+                            strArray={['A', ' ', 'C', 'r', 'e', 'a', 't', 'i', 'v', 'e', ' ', 'J', 'o', 'u', 'r', 'n', 'e', 'y']}
                             idx={15}
                         />
                     </h2>
                     <p>
-                        My fascination with technology began back in the days of Windows 96, where I honed my creative skills using Microsoft Paint. Over the years, I've evolved alongside technology, embracing new tools and techniques to stay at the forefront of the digital landscape. From Photoshop to Illustrator and even animation, I've fearlessly ventured into various creative realms. While I can't promise you a full episode of Tom and Jerry (although who knows, I might surprise you if you ask), this diverse experience has equipped me with the ability to craft visually stunning websites that prioritize both form and function.
+                        My fascination with technology began back in the days of Windows 96, where I first explored creating graphics using Microsoft Paint. This early interest in technology grew into a passion for web development as I embraced new tools and techniques to stay at the forefront of the digital landscape. Over the years, I've transitioned from designing in Photoshop and Illustrator to building interactive websites with HTML, CSS, and JavaScript. My journey has taken me from simple static pages to dynamic, responsive web applications using modern frameworks like React and Next.js. This diverse experience has not only sharpened my design skills but also my ability to create websites that seamlessly blend aesthetics with functionality.
+
                     </p>
                     <h2>
-                        <AnimatedLetters 
+                        <AnimatedLetters
                             letterClass={letterClass}
-                            strArray = {['V', 'a', 'l', 'u', 'e', 's', ' ', 'T', 'h', 'a', 't', ' ', 'D', 'r', 'i', 'v', 'e', ' ', 'M', 'e']}
+                            strArray={['V', 'a', 'l', 'u', 'e', 's', ' ', 'T', 'h', 'a', 't', ' ', 'D', 'r', 'i', 'v', 'e', ' ', 'M', 'e']}
                             idx={15}
                         />
-                    </h2>     
+                    </h2>
                     <p>
                         In my work, I hold three core values close to heart:
                     </p>
@@ -57,13 +58,13 @@ const About = () => {
                     </p>
                     <p>
                         <span>2. Efficiency:</span> I'm dedicated to optimizing processes and workflows to maximize efficiency. In the fast-paced world of technology, time is a valuable resource, and I'm committed to delivering solutions that respect both your time and the end user's experience.
-                    </p> 
+                    </p>
                     <p>
                         <span>3. Customer Service:</span> Ultimately, the success of any project hinges on the satisfaction of its users. I'm driven by a commitment to providing top-notch customer service, ensuring that the end product not only meets but exceeds expectations.
-                    </p>   
+                    </p>
                     <p>
                         Thank you for taking the time to get to know me. I'm excited to continue my journey as a full-stack developer, tackling new challenges, and creating meaningful digital experiences.
-                    </p>        
+                    </p>
                 </div>
 
                 <div className='stage-cube-cont'>
