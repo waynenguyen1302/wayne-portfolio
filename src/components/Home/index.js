bee2038023afcc6b7957caf3cf6f6c251066b4e3
waynenguyen1302 <quang.ndm91@gmail.com>
2024-05-27 22:28:56 -0400

    fix no outgoing link home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,7 +2,7 @@ import LogoTitle from '../../assets/images/logo-w2.png';
 import { Link } from 'react-router-dom';
 import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
-import React,{ useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Loader from 'react-loaders';
 import 'loaders.css/src/animations/ball-grid-beat.scss';
 import Particle from './Particles/Particles';
@@ -11,49 +11,58 @@ import Logo2 from './Logo2';
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
-    const nameArray = ['a','y','n','e']
-    const jobArray = ['w','e','b',' ','d','e','v','e','l','o','p','e','r','.']
+    const nameArray = ['a', 'y', 'n', 'e']
+    const jobArray = ['w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.']
 
     useEffect(() => {
-        
+
         let timeoutId = setTimeout(() => {
             setLetterClass('text-animate-hover')
         }, 4000)
-        
+
         return () => {
-                    clearTimeout(timeoutId)
-                }
+            clearTimeout(timeoutId)
+        }
     }, [])
 
     return (
-        <>  
+        <>
             <Particle />
-            <div className="container home-page">  
-            
+            <div className="container home-page">
+
                 <div className="text-zone">
                     <h1>
                         <span className={letterClass}>H</span>
                         <span className={`${letterClass} _12`}>i,</span>
-                        <br /> 
+                        <br />
                         <span className={`${letterClass} _13`}>I</span>
                         <span className={`${letterClass} _14`}>'m</span>
                         <img src={LogoTitle} alt="developer" />
                         <AnimatedLetters letterClass={letterClass}
-                        strArray={nameArray}
-                        idx={15}/>
+                            strArray={nameArray}
+                            idx={15} />
                         <br />
                         <AnimatedLetters letterClass={letterClass}
-                        strArray={jobArray}
-                        idx={19}/>
+                            strArray={jobArray}
+                            idx={19} />
                     </h1>
                     <h2>Fullstack Developer / Software Developer</h2>
                     <Link to="/contact" className="flat-button">CONTACT ME</Link>
+
                 </div>
                 <Logo2 />
-            </div>            
+                <div className="bottom-nav">
+                    <div className='link-list'>
+                        <><Link to="/about">About Me</Link></>
+                        <><Link to="/portfolio">Portfolio</Link></>
+                        <><Link to="/skills">Skills</Link></>
+                    </div>
+                </div>
+            </div>
+
             <Loader type="ball-grid-pulse" />
         </>
-        
+
     )
 }
 
