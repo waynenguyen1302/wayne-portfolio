028dd6d290cf11ff56ce12f2ade9f19845b748e1
waynenguyen1302 <quang.ndm91@gmail.com>
2024-05-27 22:40:12 -0400

    lazy load other pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
+import React, { Suspense, lazy } from 'react';
 import Layout from './components/Layout';
 import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-import Portfolio from './components/Portfolio';
-import Skills from './components/Skills';
+import Loader from 'react-loaders';
+
+const About = lazy(() => import('./components/About'));
+const Contact = lazy(() => import('./components/Contact'));
+const Portfolio = lazy(() => import('./components/Portfolio'));
+const Skills = lazy(() => import('./components/Skills'));
 
 function App() {
   return (
-     <Routes>
+    <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/skills" element={<Skills />} />
-      </Route>      
-     </Routes>
+        <Route
+          path="/about"
+          element={
+            <Suspense fallback={<Loader type="ball-grid-pulse" />}>
+              <About />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/contact"
+          element={
+            <Suspense fallback={<Loader type="ball-grid-pulse" />}>
+              <Contact />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/portfolio"
+          element={
+            <Suspense fallback={<Loader type="ball-grid-pulse" />}>
+              <Portfolio />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/skills"
+          element={
+            <Suspense fallback={<Loader type="ball-grid-pulse" />}>
+              <Skills />
+            </Suspense>
+          }
+        />
+      </Route>
+    </Routes>
   );
 }
 
